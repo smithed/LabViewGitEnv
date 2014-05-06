b24850f2da5e64b2811a5b260f6032df943b8b5c
Daniel Smith <daniel.smith@ni.com>
2014-05-06 10:55:27 -0500

    cleaned up code and added comments

--- bin/LVMergeWrapper.sh
+++ bin/LVMergeWrapper.sh
@@ -16,35 +16,42 @@ then
 	source ./LVConfig.sh
 fi
 
+# Repository directory in windows path notation
+WD=$(pwd | sed -e "${ENDFIX}" | sed -e "${MKWINPATH}" | sed -e  "${PATHFIX}")
+
+#The following used to have an issue in sourcetree in some cases, where the path would be ./ (remleaddot)
+#I think this makes trailfix invalid, but I wasn't sure why it didn't work in the first place so I left it
+#Pathfix is then used to make sure any repository relative paths are in windows path notation
+#Finally we take the windows form of the working dir and since the relative paths have no leading token, we just concatenate
+#This covers issues with ./blah.vi, for example, as well as \folder1\folder2\test.vi
+
+
 #BASE="${WD}\\$(echo "$1" | sed -e "${TRAILFIX}")"
 BASE=$(echo "$1" | sed -e "${REMLEADDOT}")
 BASE=$(echo "$BASE" | sed -e "${PATHFIX}")
 BASE=$(echo "$BASE" | sed -e "${TRAILFIX}")
 BASE="${WD}\\$(echo "$BASE")"
-echo $BASE
 
 #THEIRS="${WD}\\$(echo "$2" | sed -e "${TRAILFIX}")"
 THEIRS=$(echo "$2" | sed -e "${REMLEADDOT}")
 THEIRS=$(echo "$THEIRS" | sed -e "${PATHFIX}")
 THEIRS=$(echo "$THEIRS" | sed -e "${TRAILFIX}")
 THEIRS="${WD}\\$(echo "$THEIRS")"
-echo $THEIRS
 
 #YOURS="${WD}\\$(echo "$3" | sed -e  "${TRAILFIX}")"
 YOURS=$(echo "$3" | sed -e "${REMLEADDOT}")
 YOURS=$(echo "$YOURS" | sed -e "${PATHFIX}")
 YOURS=$(echo "$YOURS" | sed -e "${TRAILFIX}")
 YOURS="${WD}\\$(echo "$YOURS")"
-echo $YOURS
 
 #MERGED="${WD}\\$(echo "$4" | sed -e  "${TRAILFIX}")"
 MERGED=$(echo "$4" | sed -e "${REMLEADDOT}")
 MERGED=$(echo "$MERGED" | sed -e "${PATHFIX}")
 MERGED=$(echo "$MERGED" | sed -e "${TRAILFIX}")
 MERGED="${WD}\\$(echo "$MERGED")"
-echo $MERGED
-
-
 
 # Execute Compare
 "${LabViewShared}\LabVIEW Merge\LVMerge.exe" "${LabViewBin}" "${BASE}" "${THEIRS}" "${YOURS}" "${MERGED}"
+
+#make sure the tool doesn't delete everything before merge completed
+sleep 5
